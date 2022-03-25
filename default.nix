c14cf908241594d0aeb37ba2c37738ac56ab2eda
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-03-24 17:56:27 -1000

    mutate path

--- default.nix
+++ default.nix
@@ -1,11 +1,5 @@
 { pkgs ? import <nixpkgs> { } }:
 
-let
-  gopro-compress = pkgs.writeShellScriptBin "gopro-compress" ''
-    ${./gopro-compress} "$@"
-  '';
-in pkgs.stdenv.mkDerivation rec {
-  name = "gopro-compress";
-  dontUpack = true;
-  buildInputs = [ pkgs.ffmpeg-full gopro-compress ];
-}
+pkgs.writeShellScriptBin "gopro-compress" ''
+  PATH=$PATH:${pkgs.ffmpeg-full}/bin ${./gopro-compress} "$@"
+''
